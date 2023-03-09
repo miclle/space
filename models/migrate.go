6a1d1b7530ff2f20cf84337a4dfa65289ca52de4
Miclle Zheng <miclle.zheng@gmail.com>
2023-03-09 23:52:08 +0800

    Add model migrate

---
+++ models/migrate.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/fox-gonic/fox/database"
+)
+
+// Migrate models
+func Migrate(db *database.Database) error {
+
+	err := db.AutoMigrate(
+		&Account{},
+		&Authentication{},
+		&Space{},
+		&Page{},
+	)
+	if err != nil {
+		return err
+	}
+
+	return err
+}
